24b7f63544e95e7c0c9d1a2ed38063472f259735
boseaditya1994 <boseaditya1994@gmail.com>
2026-07-09 21:07:34 +0530

    Advanced Dagster: Using Partitions with Incremental Models

--- airbnb/models/fct/fct_reviews.sql
+++ airbnb/models/fct/fct_reviews.sql
@@ -1,19 +1,18 @@
 {{
   config(
     materialized = 'incremental',
-    on_schema_change='fail',
-    event_time='review_date'
+    on_schema_change='fail'
     )
 }}
-
 WITH src_reviews AS (
   SELECT * FROM {{ ref('src_reviews') }}
 )
-SELECT 
+SELECT
   {{ dbt_utils.generate_surrogate_key(['listing_id', 'review_date', 'reviewer_name', 'review_text']) }} as review_id,
   *
 FROM src_reviews
 WHERE review_text is not null
+
 {% if is_incremental() %}
   {% if var("start_date", False) and var("end_date", False) %}
     {{ log('Loading ' ~ this ~ ' incrementally (start_date: ' ~ var("start_date") ~ ', end_date: ' ~ var("end_date") ~ ')', info=True) }}
